96051bbf7193bfbcc6973410a3dc8220576c79f4
Viet Nguyen <vietjul@gmail.com>
2017-09-09 22:19:37 -0400

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+//populate a list of topics from an array
+
+//click on one button, show gif of that topic
+
+//click on each gif, it moves
+
+//click again, it stops
+
+//type in input box a topic, click add, topic shows in list of topics
