3e4c22776614a391f8234836e712a1b90d265e00
Viet Nguyen <vietjul@gmail.com>
2017-09-11 18:32:54 -0400

    app works, work on: decor, default input enter

--- assets/app.js
+++ assets/app.js
@@ -1,57 +1,61 @@
 $(document).ready(function() {
-	
-	var topics = ["dog", "cat", "horse", "monkey", "panda", "cute puppy", "funny dog", "bird", "rabbit", "dance", "funny cat", "funny ape", "funny robot", "kids", "sports fail", "mr bean"];
-	
-	//populate a list of topics from an array
-	for (var i = 0; i < topics.length; i++) {
-		$(".allTopics").append('<button type="button" class="btn btn-info topic">' + topics[i] + '</button>');
-		
-		//get gif from giphy
-		//MY GIPHY API KEY: e76dd1758cb74a9bb16571a9ae644b11
-		var getGif = $.get("http://api.giphy.com/v1/gifs/search?q=" + topics[i] + "&api_key=e76dd1758cb74a9bb16571a9ae644b11&limit=10");
-		
+	function toggleGif(src) {
+		var newSrc = "";
+		if (src.indexOf('.gif') != -1) { // if .gif in str
+			newSrc = src.replace("giphy.gif", "480w_s.jpg");
+		}
+		else { // if .gif not in str 
+			newSrc = src.replace("480w_s.jpg", "giphy.gif");
+		}
+		return newSrc;
 	}
 	
-	//click on one button, show gif of that topic
-	$(".topic").on("click", function() {
-		var topicName = $(this).text();
-		var chosenTopic = $.get("http://api.giphy.com/v1/gifs/search?q=" + topicName + "&api_key=e76dd1758cb74a9bb16571a9ae644b11&limit=10");
+	function addTopic(topicName) {
+		// create button
+		$newTopic = $('<button type="button" class="btn btn-info topic">' + topicName + '</button>');
 		
-		chosenTopic.done(function(obj) {
-			//clear old topic gif
-			$(".gifBoard").html("");
+		// add event handler
+		$newTopic.on("click", function() {
+			//MY GIPHY API KEY: e76dd1758cb74a9bb16571a9ae644b11
+			var chosenTopic = $.get("http://api.giphy.com/v1/gifs/search?q=" + topicName + "&api_key=e76dd1758cb74a9bb16571a9ae644b11&limit=10");
 			
-			//show new topic gif
-			for (var i = 0; i < obj.data.length; i++) {
-				$(".gifBoard").append('<div class="gif"><img src="' + obj.data[i].images["480w_still"].url + '"/></div>');
-			}
-			
-			//img to gif	
-			$(".gif").on("click", function() {
-				var str = $(this).children('img').attr('src');
-				var res = str.replace("480w_s.jpg", "giphy.gif");
+			chosenTopic.done(function(obj) {
+				//clear old topic gif
+				$(".gifBoard").html("");
 				
-				$(this).html('<img src="' + res + '"/>');
-			});	
-			
-			//gif to img? toggle?
-			
-			console.log("gif retrieved: ", obj.data);
+				//show new topic gif
+				for (var i = 0; i < obj.data.length; i++) {
+					$(".gifBoard").append('<div class="gif"><img src="' + obj.data[i].images["480w_still"].url + '"/></div>');
+				}
+				
+				//img to gif	
+				$(".gif").on("click", function() {
+					var src = $(this).children('img').attr('src');
+					var res = toggleGif(src);
+					$(this).html('<img src="' + res + '"/>');
+				});
+			});
 		});
-	});
+		
+		$(".allTopics").append($newTopic);
+	}
+	
+	var topics = ["dog", "cat", "horse", "monkey", "panda", "cute puppy", "funny dog", "bird", "rabbit", "dance", "funny cat", "funny ape", "funny robot", "kids", "sports fail", "mr bean"];
+
+	//populate a list of topics from array topics
+	for (var i = 0; i < topics.length; i++) {
+		addTopic(topics[i]);	
+	}
 
 	//type in input box a topic, click add, topic shows in list of topics
 	$(".addTopic").on("click", function() {
 		var newTopic = $("input").val();
 		
 		if (newTopic) {
-			$(".allTopics").append('<button type="button" class="btn btn-info topic">' + newTopic + '</button>');
+			addTopic(newTopic);
 		}
 		else {
 			return;
 		}
-		
-		console.log("new topic: ", newTopic);
 	});
-
 });
