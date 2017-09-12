0e90648eeeb8d8d20e61b15da8ee59c0bcd53b47
Viet Nguyen <vietjul@gmail.com>
2017-09-11 23:14:36 -0400

    work on default enter refresh, decor

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 	function toggleGif(src) {
 		var newSrc = "";
+		
 		if (src.indexOf('.gif') != -1) {
 			newSrc = src.replace("giphy.gif", "480w_s.jpg");
 		}
 		else {
 			newSrc = src.replace("480w_s.jpg", "giphy.gif");
 		}
+		
 		return newSrc;
 	}
 	
@@ -19,16 +21,16 @@ $(document).ready(function() {
 			//MY GIPHY API KEY: e76dd1758cb74a9bb16571a9ae644b11
 			var chosenTopic = $.get("http://api.giphy.com/v1/gifs/search?q=" + topicName + "&api_key=e76dd1758cb74a9bb16571a9ae644b11&limit=10");
 			
-			chosenTopic.done(function(obj) {
+			chosenTopic.done(function(response) {
 				//clear old topic gif
 				$(".gifBoard").html("");
 				
 				//show new topic gif
-				for (var i = 0; i < obj.data.length; i++) {
-					$(".gifBoard").append('<div class="gif"><img src="' + obj.data[i].images["480w_still"].url + '"/></div>');
+				for (var i = 0; i < response.data.length; i++) {
+					$(".gifBoard").append('<div class="gif"><img src="' + response.data[i].images["480w_still"].url + '"/></div>');
 				}
 				
-				//img to gif	
+				//img to gif, use attr data-state still	
 				$(".gif").on("click", function() {
 					var src = $(this).children('img').attr('src');
 					var res = toggleGif(src);
@@ -39,6 +41,8 @@ $(document).ready(function() {
 		
 		$(".allTopics").append($newTopic);
 	}
+	
+	//add function stop default enter key press: onkeyup
 	
 	//default topics
 	var topics = ["dog", "cat", "horse", "monkey", "panda", "cute puppy", "funny dog", "bird", "rabbit", "dance", "funny cat", "funny ape", "funny robot", "kids", "sports fail", "mr bean"];
